71536d7b04cfce7750b00c3ea94f2eec46470671
Cory Imel <cory.imel@gmail.com>
2017-10-11 15:48:57 -0500

    Added Prompt exercises

---
+++ js/external.js
@@ -0,0 +1,5 @@
+console.log('Hello from External JavaScript!');
+
+alert('Welcome to my Website!');
+var favColor = prompt("What is your favorite colo? ");
+alert('Great! ' + favColor + " is my favorite color too!")
