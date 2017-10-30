f11c5b53abffe6a3dc427507f3bdb4eef0b867a8
Cory Imel <cory.imel@gmail.com>
2017-10-30 10:54:15 -0500

    Added initial JQuery Exercises

---
+++ js/objects-warmup.js
@@ -0,0 +1,21 @@
+// ---- EXERCISE 1
+// 1. Write a function, deactivateMembers()
+// to take in the following array of objects as an input:
+
+    var members = [
+        {
+            joined: 2009,
+            name: "John",
+            activeMember: true
+        },
+        {
+            joined: 2011,
+            name: "Sally",
+            activeMember: true
+        },
+        {
+            joined: 2013,
+            name: "John",
+            activeMember: true
+        }
+    ];
